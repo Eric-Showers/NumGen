0c1ae32c429843a8a95d0f1526f2caac00d9d23d
E. Showers <E. Showers>
2017-09-19 16:03:02 -0700

    Commented structural ideas and added basic logic for year roll over

--- ISRCGen/isrc_generator.py
+++ ISRCGen/isrc_generator.py
@@ -10,21 +10,26 @@ class isrc_generator:
     def generate(self):
         now = datetime.datetime.now()
         curYear = now.year % 100        #Retrieves two digit year format
-        curYear = str(curYear)
 
+        #Accesses storage of previously assigned codes and finds last assigned code
         codeLibrary = open('ExistingCodes.txt', 'r+')
         for lines in codeLibrary:
-            #print(lines)
             prevID = lines
-        #print('yeahboi')
-        print(prevID)
+        
+        prevYear = int(prevID[5]+prevID[6])
+
+        #Extracts last 5 digits of code and increments it's value by 1.
+        #Then casts as String and ensures that it is 5 characters, adding 0's as needed
         newID = int(prevID[7:]) + 1
-        print(newID)
         newID = str(newID).zfill(5)
 
+        if prevYear < curYear:
+            newID = '00001'
+        curYear = str(curYear)
+
+        #Builds the code by concatenating country, org, year, and product codes
         code = ''
         code = code + self.country + self.org + curYear + newID
-        print(code)
         codeLibrary.write('\n' + code)
         codeLibrary.close()
 
